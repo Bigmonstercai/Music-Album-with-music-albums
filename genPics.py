74d2cb26b42bc5fd566bfbda36cbb378c0e2938f
Bigmonstercai <Bigmonstercai@qq.com>
2015-09-22 00:35:33 +0800

    readAPIC
    Now can read Album Cover successfully

--- genPics.py
+++ genPics.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-import shutil
 from readAPIC import readAPIC
 
 path = 'E:\Bigmonstercai\Music'
@@ -10,11 +9,17 @@ for artist in os.listdir(path):
     if os.path.isdir(path + '\\' + artist):
         for album in os.listdir(path + '\\' + artist):
             if os.path.isdir(path + '\\' + artist + '\\' + album):
-                # print((artist + album).encode('utf-8','ignore').decode('GB18030','ignore'))
-                # readAPIC(path + '\\' + artist + '\\' + album + '\\' + os.listdir(path + '\\' + artist + '\\' + album)[0], artist, album)
-                if os.path.exists(path + '\\' + artist + '\\' + album + '\\folder.jpg'):
-                    shutil.copyfile(
-                        path + '\\' + artist + '\\' + album + '\\folder.jpg', 'images\\' + artist + '-' + album + '.jpg')
-                else:
-                    readAPIC(path + '\\' + artist + '\\' + album + '\\' +
-                             os.listdir(path + '\\' + artist + '\\' + album)[0], artist, album)
+                i = 0
+                while True:
+                    try:
+                        firstfile = os.listdir(
+                            path + '\\' + artist + '\\' + album)[i]
+                        filename = path + '\\' + artist + \
+                            '\\' + album + '\\' + firstfile
+                        filetype = os.path.splitext(firstfile)[1]
+                        if readAPIC(filename, artist, album, filetype):
+                            break
+                        else:
+                            i = i + 1
+                    except IndexError:
+                        print(path + '\\' + artist + '\\' + album)
--- readAPIC.py
+++ readAPIC.py
@@ -1,34 +1,38 @@
-def readAPIC(filename, artist, album):
+def readAPIC(filename, artist, album, filetype):
     fp = open(filename, 'rb')
-    valid = 0
-    pic = []
-
-    while True:
-        print(filename, artist, album)
-        a = fp.read(2)
-        print(a)
-        if valid == 0:
-            if a == b'\xFF\xD8':
-                valid = 1
-                pic.append(a)
-            elif a == '':
-                break
-        elif valid == 1:
-            pic.append(a)
-            if a == b'\xFF\xD9':
-                valid = 0
-                break
-            elif a == '':
-                valid = 0
-                break
-
-    fp.close
-
-    if pic == []:
-        return False
+    if filetype == '.m4a':
+        covr = b'\x63\x6F\x76\x72'
+    elif filetype == '.mp3':
+        covr = b'ID3'
     else:
-        fo = open('images\\' + artist + '-' + album + '.jpg', 'wb')
-        for i in pic:
-            fo.write(i)
-        fo.close
-        return True
+        return False
+    imagetype = '.png'
+    start = b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A'  # 默认为png,因为png的文件头长，误匹配到的概率低
+    end = b'\x00\x00\x00\x00\x49\x45\x4E\x44\xAE\x42\x60\x82'
+    a = fp.read()
+    covr_num = a.find(covr)
+    a = a[covr_num: -1]
+    start_num = a.find(start)
+    end_num = a.find(end)
+    if start_num == -1:  # 不为png则为jpg
+        start = b'\xFF\xD8'
+        end = b'\xFF\xD9'
+        start_num = a.find(start)
+        end_num = a.find(end)
+        imagetype = '.jpg'
+
+    if imagetype == '.jpg':
+        pic = a[start_num: end_num + 2]
+        while pic[2: -1].find(start) != -1:
+            pic = pic[pic[2: -1].find(start) + 2:-1]
+    elif imagetype == '.png':
+        pic = a[start_num: end_num + 12]
+        while pic[8: -1].find(start) != -1:
+            pic = pic[pic[8: -1].find(start) + 8:-1]
+
+    fo = open('images/' + artist + '-' + album + imagetype, 'wb')
+    fo.write(pic)
+
+    fp.close()
+    fo.close()
+    return True
